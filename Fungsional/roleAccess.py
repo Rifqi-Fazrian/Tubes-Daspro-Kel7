d5d31a1a5044b3765b9b9bbbb4e5ce672e60ad87
Kean1202 <88710618+Kean1202@users.noreply.github.com>
2022-04-14 00:16:32 +0700

    isi pembagian akses user dan admin

---
+++ Fungsional/roleAccess.py
@@ -0,0 +1,4 @@
+admin = ["register", "login", "add_game_to_store", "change_game_info", "change_game_stock", "list_games",
+         "search_games", "top_up", "help", "save"]
+
+user = ["login", "list_games", "buy_games", "see_owned_games", "search_owned_games", "search_store", "receipts", "help", "save"]
